01bf662dc8ec77877162e4c26486953ce7fe14a8
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-04-06 11:41:56 -0700

    just finshing up the Queue class

--- index.js
+++ index.js
@@ -29,3 +29,36 @@ console.log(stack.pop()); // 'llama'
 console.log(stack.peek()); // 'fox'
 console.log(stack.pop()); // 'fox'
 console.log(stack.pop()); // null
+
+class Queue {
+  constructor() {
+    this.arr = [];
+  }
+  //adds item
+  enqueue(item) {
+    return (this.arr = [...this.arr, item]);
+  }
+  //return the next item in line
+  dequeue() {
+    return this.arr.splice(0, 1);
+  }
+  //checks ti see if there is a next one
+  hasNext() {
+    return !!this.arr[0];
+  }
+}
+
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue);
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
