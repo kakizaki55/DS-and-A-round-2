eee2a3e9cb2fc36420d9e135d5e8f7590ca20e88
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-04-06 11:54:52 -0700

    done with anagrams

--- index.js
+++ index.js
@@ -62,3 +62,24 @@ console.log(queue.hasNext()); // true
 console.log(queue.dequeue()); // 'llama'
 console.log(queue.hasNext()); // false
 console.log(queue.dequeue()); // null
+
+
+
+onst word1 = 'superintended';
+const word2 = 'unpredestined';
+const word3 = 'pictorialness';
+const word4 = 'documentarily';
+
+function anagrams(wordOne, wordTwo) {
+  const word1 = wordOne.split('').sort().join('');
+  const word2 = wordTwo.split('').sort().join('');
+
+  if (word1 === word2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(anagrams(word1, word2));
+console.log(anagrams(word3, word4));
